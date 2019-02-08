8e3c59bb37e1b9149a556d296f42543b2e74d4ba
Luke Poutney <2314348p@student.gla.ac.uk>
2019-02-08 17:56:49 +0000

    Completed up to Chapter 10.7

--- tango_with_django_project/settings.py
+++ tango_with_django_project/settings.py
@@ -17,6 +17,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates')
 STATIC_DIR = os.path.join(BASE_DIR, 'static')
 MEDIA_DIR = os.path.join(BASE_DIR, 'media')
+SESSION_EXPIRE_AT_BROWSER_CLOSE = True
 
 
 MEDIA_ROOT = MEDIA_DIR
@@ -131,4 +132,5 @@ STATICFILES_DIRS = [STATIC_DIR, ]
 
 STATIC_URL = '/static/'
 
-LOGIN_URL = '/rango/login/'
+LOGIN_URL = '/rango/login/'
+
